7b3d52c7cc387d9088661f309618d5c54030c037
André Malta <andrmalta@gmail.com>
2022-02-09 23:35:51 -0300

    Changing port

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,3 +1,3 @@
 docker stop nginx-api-gateway
 docker rm nginx-api-gateway
-docker run -d -p 4000:4000 --name nginx-api-gateway nginx-gateway
+docker run -d -p 5000:5000 --name nginx-api-gateway nginx-gateway
